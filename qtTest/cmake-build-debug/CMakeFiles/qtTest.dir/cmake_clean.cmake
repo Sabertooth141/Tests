3640cbdbcbe5dec40637a7938660dbb9bbb14554
Sabertooth141 <44132181+Sabertooth141@users.noreply.github.com>
2024-04-03 23:56:57 -0400

    update

---
+++ qtTest/cmake-build-debug/CMakeFiles/qtTest.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/qtTest_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/qtTest_autogen.dir/ParseCache.txt"
+  "qtTest_autogen"
+  "CMakeFiles/qtTest.dir/main.cpp.obj"
+  "CMakeFiles/qtTest.dir/main.cpp.obj.d"
+  "CMakeFiles/qtTest.dir/qtTest_autogen/mocs_compilation.cpp.obj"
+  "CMakeFiles/qtTest.dir/qtTest_autogen/mocs_compilation.cpp.obj.d"
+  "libqtTest.dll.a"
+  "qtTest.exe"
+  "qtTest.exe.manifest"
+  "qtTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/qtTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ qtTest/cmake-build-debug/CMakeFiles/qtTest_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/qtTest_autogen"
+  "qtTest_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/qtTest_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
